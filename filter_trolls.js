4313f0a4ca0915ce7fea6416399240a174302ad4
John Collins <john.collins@braincorp.com>
2020-12-23 00:49:40 -0800

    More trolls. fixes.

--- filter_trolls.js
+++ filter_trolls.js
@@ -1,9 +1,12 @@
 var elems = document.getElementsByClassName("commentbody");
+var deletions = [];
 for (var i = 0; i < elems.length; i++) {
   var elem = elems[i];
   var name = elem.childNodes[2].textContent.trim();
-  if (name == "xu" || name == "sarah" || name == "bob" || name == "harry" ) {
-    console.log("Deleting troll comment: ", elem);
-    elem.remove();
+  if (name == "xu" || name == "sarah" || name == "bob" || name == "harry" || name == "jayne" || name == "janice") {
+    console.log("Deleting troll comment from ${name}: ", elem);
+    deletions.push(elem);
   }
 };
+
+deletions.forEach((e => e.remove()));
