18d38e9d498e2b543f309878aad570a6eae95f11
John Collins <john.collins@braincorp.com>
2020-12-22 23:58:53 -0800

    Simple

--- popup.js
+++ popup.js
@@ -2,14 +2,8 @@ var elems = document.getElementsByClassName("commentbody");
 for (var i = 0; i < elems.length; i++) {
   var elem = elems[i];
   var name = elem.childNodes[2].textContent.trim();
-  console.log(name);
-  if (name == "xu" || name == "sarah" || name == "bob" || name == "henry" ) {
-    console.log("Deleting!");
+  if (name == "xu" || name == "sarah" || name == "bob" || name == "harry" ) {
+    console.log("Deleting troll comment: ", elem);
     elem.remove();
   }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  console.log(document.getElementsByClassName("commentbody"));
-}, false);
+};
